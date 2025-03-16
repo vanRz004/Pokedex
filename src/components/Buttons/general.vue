1c48b79902be8eeb0f69d505296f7747877790b3
Vanessa Ramirez <anvramirez004@gmail.com>
2025-03-16 00:01:58 -0500

    Home view and general css

---
+++ src/components/Buttons/general.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps(['title']);
+
+</script>
+<template>
+<div>
+  <button class="general-btn">{{ props.title }}</button>
+</div>
+</template>
+<style scoped>
+.general-btn{
+  background-color: #F22539;
+  color: #F9F9F9;
+  border-radius: 60px;
+  width: 131px;
+  height: 44px;
+  border: 0;
+  font-size: 18px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,52 @@
+<script setup>
+import GeneralBtn from '@/components/Buttons/general.vue';
+
+
+</script>
+
+<template>
+  <main>
+    <div class="image-container">
+      <img class="background" src="/src/assets/images/back-character.png" alt="Background">
+      <img class="character" src="/src/assets/images/character.png" alt="Character">
+    </div>
+    <h1> Welcome to Pokédex</h1>
+    <h2>The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the Pokémon world.</h2>
+    <GeneralBtn title="Get started"></GeneralBtn>
+  </main>
+</template>
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center; 
+  justify-content: center; 
+  height: 100vh; 
+  text-align: center; 
+  /* padding: 20px;  */
+}
+.image-container {
+  position: relative;
+  width: 325px; 
+  height: 288px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.background {
+  width: 206px;
+  height: 206px;
+  position: absolute;
+  z-index: 1; 
+  top: 70px;
+  right: 70px;
+}
+
+.character {
+  width: 325px; 
+  height: auto;
+  position: absolute;
+  z-index: 2; 
+}
+</style>
